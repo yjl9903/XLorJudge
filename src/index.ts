7fbfe0f98f475aa13371a01ca16c54d63edb9a82
X1 Carbon <yjl9903@vip.qq.com>
2019-10-24 19:01:16 +0800

    change: log

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './app';
 
 const server = app.listen(app.get('port'), '0.0.0.0', () => {
   console.log(
-    'Judger is running at http://localhost:%d in %s mode',
+    '\n  XLorJudge is running at http://localhost:%d in %s mode\n',
     app.get('port'),
     app.get('env')
   );
