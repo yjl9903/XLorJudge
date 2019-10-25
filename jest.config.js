f6f8e190c67565df21cbff9d493124501056f694
X1 Carbon <yjl9903@vip.qq.com>
2019-10-25 17:58:36 +0800

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,24 @@
 module.exports = {
   globals: {
-      "ts-jest": {
-          tsConfig: "tsconfig.json"
-      }
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
   },
   moduleFileExtensions: [
-      "ts",
-      "js"
+    "ts", "js"
+  ],
+  moduleNameMapper: {
+    "^@config$": "<rootDir>/src/configs/index.ts",
+    "^@util$": "<rootDir>/src/util.ts",
+  },
+  moduleDirectories: [
+    "node_modules", "src/types"
   ],
   transform: {
-      "^.+\\.(ts|tsx)$": "ts-jest"
+    "^.+\\.(ts|tsx)$": "ts-jest"
   },
   testMatch: [
-      "**/test/**/*.test.(ts|js)"
+    "**/test/**/*.test.(ts|js)"
   ],
   testEnvironment: "node"
 };
