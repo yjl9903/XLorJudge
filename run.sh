aed52fd781e4b1e8b594d5cf471b0c6cca6908c8
X1 Carbon <yjl9903@vip.qq.com>
2019-10-27 21:44:13 +0800

    update: run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,13 @@ if [ `id -u` -ne 0 ]; then
     exit 1
 fi
 
-# core=$(grep --count ^processor /proc/cpuinfo)
-# n=$(($core*2))
-
 mkdir -p /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
 chown root -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
 chgrp root -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
 
 chown compiler -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
 chgrp compiler -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
-chown compiler:compiler /Judge/dist/run/temp /Judge/dist/run/sub /Judge/dist/run/checker
+chown compiler:compiler /judge/run/temp /judge/run/sub /judge/run/checker
 
 memcached -p 11211 -u compiler -m 64m -d
-npm run start
+yarn run start
