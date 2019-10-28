184495fbb9d6d841fdcb9cbdfca40bb13d7f17ba
X1 Carbon <yjl9903@vip.qq.com>
2019-10-28 17:15:52 +0800

    remove: no use dep

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,4 +1,5 @@
 import { promisify } from 'util';
+import EventEmitter from 'events';
 import { createClient } from 'redis';
 
 import { Verdict } from '../verdict';
@@ -31,17 +32,24 @@ export type ResultMsg = {
   time: number;
   memory: number;
 };
-type Msg = WatingMsg | ErrorMsg | JudgeMsg | ResultMsg;
+export type Msg = WatingMsg | ErrorMsg | JudgeMsg | ResultMsg;
+
+const Bus = new EventEmitter();
 
 export function update(token: string, msg: Msg) {
   const str = JSON.stringify(msg);
   client.set(token, str, 'EX', 3600 * 24);
+  Bus.emit(token, msg);
 }
 
 export async function query(token: string): Promise<Msg> {
   return JSON.parse(await get(token)) as Msg;
 }
 
-export function subscribe(token: string, fn: () => void) {}
+export function subscribe(token: string, fn: (msg: Msg) => void) {
+  Bus.addListener(token, fn);
+}
 
-export function unsubscribe(token: string) {}
+export function unsubscribe(token: string, fn: (msg: Msg) => void) {
+  Bus.removeListener(token, fn);
+}
