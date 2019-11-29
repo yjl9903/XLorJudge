9bba9a4c10e7a77fda644e764a6be9a8a5fd9f32
X1 Carbon <yjl9903@vip.qq.com>
2019-11-29 13:40:47 +0800

    fix: validator test

--- scripts/index.js
+++ scripts/index.js
@@ -151,7 +151,7 @@ const CaseNum = 5;
   });
   casesAB.push(id);
 
-  console.log(`\nStep 5: Validate test`);
+  console.log(`\nStep 5: Validator test`);
 
   tasks.splice(0, tasks.length);
   for (const id of casesAB) {
