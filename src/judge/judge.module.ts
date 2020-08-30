ecbff8eb764db1fc5a4ffa9ba556eeac91c32fe0
XLor <yjl9903@vip.qq.com>
2020-08-31 00:18:35 +0800

    remove: ws gateway

--- src/judge/judge.module.ts
+++ src/judge/judge.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JudgeService } from './judge.service';
 import { JudgeController } from './judge.controller';
-import { JudgeGateway } from './judge.gateway';
 
 @Module({
-  providers: [JudgeService, JudgeGateway],
+  providers: [JudgeService],
   controllers: [JudgeController]
 })
 export class JudgeModule {}
