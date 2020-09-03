7324a79d778328fcc405390efdd25042f8a594b0
XLor <yjl9903@vip.qq.com>
2020-09-03 13:19:37 +0800

    fix: test

--- src/polygon/polygon.service.spec.ts
+++ src/polygon/polygon.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PolygonService } from './polygon.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('PolygonService', () => {
   let service: PolygonService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PolygonService]
+      providers: [PolygonService, ConfigService]
     }).compile();
 
     service = module.get<PolygonService>(PolygonService);
