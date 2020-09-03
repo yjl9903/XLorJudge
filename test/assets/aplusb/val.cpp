ea608e477177772e1baa48e061a035f2ef2ade3d
XLor <yjl9903@vip.qq.com>
2020-09-03 13:09:34 +0800

    fix: val.cpp

--- test/assets/aplusb/val.cpp
+++ test/assets/aplusb/val.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
   registerValidation(argc, argv);
-  inf.readInt(0, 100000, "a");
+  inf.readInt(0, 1000000000, "a");
   inf.readSpace();
-  inf.readInt(0, 100000, "a");
+  inf.readInt(0, 1000000000, "b");
   inf.readEoln();
   inf.readEof();
 }
