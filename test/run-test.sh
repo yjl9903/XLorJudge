fa9e1fd0fb866054088e00a78fb8931ecf88852b
XLor <yjl9903@vip.qq.com>
2020-09-08 00:05:56 +0800

    add: test docker image

---
+++ test/run-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ `id -u` -ne 0 ]; then
+    echo "Please re-run ${this_file} as root."
+    exit 1
+fi
+
+mkdir -p /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
+chown root -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
+chgrp root -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
+
+chown compiler -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
+chgrp compiler -R /sys/fs/cgroup/memory/NSJAIL /sys/fs/cgroup/cpu/NSJAIL /sys/fs/cgroup/pids/NSJAIL
+
+chown compiler:compiler ./run/temp ./run/submission
+chown compiler:compiler ./run/checker ./run/interactor ./run/generator ./run/validator
+
+yarn run test:core
+
+yarn run test:polygon
